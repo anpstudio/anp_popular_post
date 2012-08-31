ab0339560ed5018a5c3f3b1a26db10b23ec139de
antocara <antocara@gmail.com>
2012-08-31 18:44:16 +0200

    Actualizado con nuevas funciones, nº post y gama colores
    Actualizado incluyendo do nuevas funciones:
    * Poder elegir el número de post a mostrar con un máximo de 10
    * Poder elegir entre 4 paletas de colores, Estándar, Rojo, Verde, Azul

--- anp_popular_post.php
+++ anp_popular_post.php
@@ -39,8 +39,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  **/		
 class anp_popular_post_widget extends WP_Widget {
+		
 
-	public function __construct() {
+		public function __construct() {
 			parent::__construct(
 		 		'anp_post_popular_post', // Base ID
 				'anp Popular Post', // Name
@@ -51,9 +52,11 @@ class anp_popular_post_widget extends WP_Widget {
 	 * Front-end  widget.
 	 *
 	 */
-	public function widget( $args, $instance ) {
+		public function widget( $args, $instance ) {
 			extract( $args );
 			$title = apply_filters( 'widget_title', $instance['title'] );
+			$anp_num_post= $instance['anp_num_post'];
+			$anp_colores_post = $instance['anp_colores_post'];
 			global $post, $wpdb;
 	
 			echo $before_widget;
@@ -62,7 +65,7 @@ class anp_popular_post_widget extends WP_Widget {
 			?>
 				<ul class="num_post_list">
 					<?php 
-						$result = $wpdb->get_results("SELECT comment_count,ID,post_title FROM $wpdb->posts ORDER BY comment_count DESC LIMIT 0 , 5");
+						$result = $wpdb->get_results("SELECT comment_count,ID,post_title FROM $wpdb->posts ORDER BY comment_count DESC LIMIT 0 , $anp_num_post");
 						$post_nu_clas=1;
 					foreach ($result as $post) {
 						setup_postdata($post);
@@ -84,36 +87,111 @@ class anp_popular_post_widget extends WP_Widget {
 				<?php 
 				
 			echo $after_widget;
+			
+				
+				/******
+				 * cargo hojas d estilo en función de la elección
+				 ******/
+				 
+				switch ($anp_colores_post){
+				
+				 	case 1:
+				 
+					 // añado al head hoja de estilos para shortcode  
+				     wp_register_style( 'anp_estilos', plugins_url() . '/anp_popular_post/css/anp_popular_post.css' );  
+				     wp_enqueue_style( 'anp_estilos' ); 
+				     
+				    break;  
+				    case 2:
+				    
+				    	 // añado al head hoja de estilos para shortcode  
+				        wp_register_style( 'anp_estilos_azul', plugins_url() . '/anp_popular_post/css/anp_popular_post_azul.css' );  
+				        wp_enqueue_style( 'anp_estilos_azul' ); 
+				        
+				    break; 
+				    case 3:
+				    
+				    	 // añado al head hoja de estilos para shortcode  
+				        wp_register_style( 'anp_estilos_verde', plugins_url() . '/anp_popular_post/css/anp_popular_post_verde.css' );  
+				        wp_enqueue_style( 'anp_estilos_verde' ); 
+				        
+				    break;   
+				 	case 4:
+				 
+				 	 // añado al head hoja de estilos para shortcode  
+				     wp_register_style( 'anp_estilos_rojo', plugins_url() . '/anp_popular_post/css/anp_popular_post_rojo.css' );  
+				     wp_enqueue_style( 'anp_estilos_rojo' ); 
+				     
+				    break; 
+				
+				}
 		}
 	/**
      * Sanitize widget form values as they are saved.
 	 *
 	 */
-	public function update($new_instance, $old_instance){
-		$instance = array();
-				$instance['title'] = strip_tags( $new_instance['title'] );
-		
-				return $instance;
-		
-	}
+		public function update($new_instance, $old_instance) {                
+		    return $new_instance;
+		}
 	/**
 	 * backend widget
 	 *
 	 */ 
-	public function form($instance){
+		public function form($instance){
 		
-				if ( isset( $instance[ 'title' ] ) ) {
-						$title = $instance[ 'title' ];
-					}else {
-						$title = __( 'New title', 'anp_text_popular_post' );
+					$title = esc_attr($instance['title']);
+					$anp_num_post = esc_attr($instance['anp_num_post']);
+					$anp_colores_post = esc_attr($instance['anp_colores_post']);
+					
+					?>
+				    <p>
+				        <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Titulo:','anp_creative'); ?></label>
+				        <input type="text" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" />
+				    </p>
+				   	<p>
+				        <label for="<?php echo $this->get_field_id('anp_num_post'); ?>"><?php _e('Número de post a mostrar:','anp_creative'); ?></label>
+				        <select name="<?php echo $this->get_field_name('anp_num_post'); ?>" class="widefat" id="<?php echo $this->get_field_id('anp_num_post'); ?>">
+				            <?php for ( $i = 1; $i <= 10; $i += 1) { ?>
+				            <option value="<?php echo $i; ?>" <?php if($anp_num_post == $i){ echo "selected='selected'";} ?>><?php echo $i; ?></option>
+				            <?php } ?>
+				        </select>
+				    </p>
+				    <p>
+				        <label for="<?php echo $this->get_field_id('anp_colores_post'); ?>"><?php _e('Paleta de colores:','anp_creative'); ?></label>
+				        <select name="<?php echo $this->get_field_name('anp_colores_post'); ?>" class="widefat" id="<?php echo $this->get_field_id('anp_colores_post'); ?>">
+				            <?php for ( $i = 1; $i <= 4; $i += 1) { ?>
+				            <option value="<?php echo $i; ?>" <?php if($anp_colores_post == $i){ echo "selected='selected'";} ?>>
+
+				           		<?php 
+				           			switch ($i) {
+				           				case 1:
+				           				echo 'Normal';
+				           				break;
+				           				
+				           				case 2:
+				           				echo 'Azul';
+				           				break;
+				           				
+				           				case 3:
+				           				echo 'Verde';
+				           				break;
+				           				
+				           				case 4:
+				           				echo 'Rojo';
+				           				break;	
+				           						
+				           			};
+				           		
+				           		 ?>
+				           	</option>
+				            <?php } ?>
+				        </select>
+				    </p>
+				 
+					<?php
 				}
-				?>
-				<p>
-				<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-				</p>
-				<?php 
-			}
+				
+				
 
 }
 
@@ -124,17 +202,27 @@ function registro_anp_popular_post()
 }
 add_action('widgets_init', 'registro_anp_popular_post');
 
+
+
+
 /**
  *
  * Cargo hoja de stilos para el widget
  *
  */
+ 	
+ 	
+ 	if ($anp_num_post =="5"){
+ 	 	
+ 	function anp_popular_post_estilos() { 
+ 		 // añado al head hoja de estilos para shortcode  
+ 	     wp_register_style( 'anp_estilos', plugins_url() . '/anp_popular_post/css/anp_popular_post.css' );  
+ 	     wp_enqueue_style( 'anp_estilos' ); 
+ 	     
+ 	       
+ 	 } 
+ 	 add_action('wp_head', 'anp_popular_post_estilos');
+ 	
+ 	}
 
-function anp_popular_post_estilos() { 
-	 // añado al head hoja de estilos para shortcode  
-     wp_register_style( 'anp_estilos', plugins_url() . '/anp_popular_post/css/anp_popular_post.css' );  
-     wp_enqueue_style( 'anp_estilos' ); 
-       
- } 
- add_action('wp_head', 'anp_popular_post_estilos');
 ?>
